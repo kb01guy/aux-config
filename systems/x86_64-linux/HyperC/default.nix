f9ae75dbb7af5900df67ad933c41348572125f34
kB01 <kb01@kb-one.de>
2024-08-13 00:20:34 +0200

    Added KDE Connect

--- systems/x86_64-linux/HyperC/default.nix
+++ systems/x86_64-linux/HyperC/default.nix
@@ -166,6 +166,8 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  programs.kdeconnect.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [

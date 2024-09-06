822f882f2f627ae216d921727538221c93ffb48a
kB01 <kb01@kb-one.de>
2024-09-06 17:06:54 +0200

    Fixed Typo

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.follows = "nixpkgs";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs:

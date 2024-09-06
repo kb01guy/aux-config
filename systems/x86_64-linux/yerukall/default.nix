3525122d7cc0eb097b1a49235f1e21880c0c0711
kB01 <kb01@kb-one.de>
2024-09-06 17:31:57 +0200

    Changed Sops structure, added Sops Key

--- systems/x86_64-linux/yerukall/default.nix
+++ systems/x86_64-linux/yerukall/default.nix
@@ -28,6 +28,7 @@ in {
   # Configure Secret Management
   sops.defaultSopsFile = ./secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
+  sops.age.keyFile = "/home/user/.config/sops/age/keys.txt";
   sops.secrets.example-key = {};
   sops.secrets."myservice/my_subdir/my_secret" = {};
 

20efe8ed84e3eac75a3fcc71a69f3ec8603260a3
kB01 <kB01@kb-one.de>
2024-11-21 16:16:14 +0100

    Added Luckperms Plugin, Symlink structure only links .jar File (Issue with Readonly Filesystem if plugins-folder is in nix store)

--- modules/nixos/services/minecraft/survival.nix
+++ modules/nixos/services/minecraft/survival.nix
@@ -24,8 +24,9 @@ in
         kB01guy = "1ff88b66-beda-4386-85b9-a00a5c27437a";
       };
       openFirewall = true;
-      symlinks.plugins = pkgs.linkFarmFromDrvs "plugins" (builtins.attrValues {
-      });
+      symlinks = {
+        "plugins/LuckPerms.jar" = pkgs.fetchurl { url = "https://download.luckperms.net/1561/bukkit/loader/LuckPerms-Bukkit-5.4.146.jar"; sha512 = "3yx163xas6g30crj41ad8j9gh55ygfh7vbaq12hlm4rxf1npnxh95rhn2nx0qcjd4nl1rz8f8pbvmlh6ka32ahvn6x9rxsc8g6v24jz"; };
+      };
     };
   };
 }

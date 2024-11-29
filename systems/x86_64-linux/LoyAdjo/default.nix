4b1aa15e356eb0dbcd1b114ec4d5fcdaf14c7ab0
kB01 <kb01@kb-one.de>
2024-11-29 18:53:10 +0100

    Enabled SSH-Agent in System Configuration

--- systems/x86_64-linux/LoyAdjo/default.nix
+++ systems/x86_64-linux/LoyAdjo/default.nix
@@ -83,6 +83,7 @@
   # services.xserver.displayManager.autoLogin.user = "kb";
 
 #  services.gpg-agent.enable = true;
+  programs.ssh.startAgent = true;
 
   # List packages installed in system profile. To search, run: $ nix search wget
   environment.systemPackages = with pkgs; [
